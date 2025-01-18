e6094e76b339c70578f6e565552c805e6d52789b
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2025-01-18 15:16:15 -0500

    book module is back + fix buggy hackerperspective

--- frontend/src/components/hacker-side/hackerperspective.jsx
+++ frontend/src/components/hacker-side/hackerperspective.jsx
@@ -1,35 +1,4 @@
 import React from 'react'
-import Navbar from '../nav/navbar';
-import { useState, useEffect } from 'react';
-import Statistics from './statistics';
-import Terminal from './terminal';
-import Leaderboard from './leaderboard';
-
-const HackerPerspective = () => {
-  const [page, setPage] = useState("statistics"); // Default page
-
-  const renderPage = () => {
-    switch (page) {
-      case "statistics":
-        return <Statistics />;
-      case "terminal":
-        return <Terminal />;
-      case "leaderboard":
-        return <Leaderboard />;
-    }
-  };
-
-  return (
-    <div>
-      <Navbar setPage={setPage} />
-      <div className="page-content">{renderPage()}</div>
-    </div>
-  );
-  
-}
-
-export default HackerPerspective
-import React from 'react'
 import { motion } from 'framer-motion';
 import Navbar from '../nav/navbar';
 import { useState } from 'react';
