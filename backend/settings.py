b1a8947dfc7fe0298d17bbe6f11490486a08e030
Chrisyhjiang <chrisyh.jiang@gmail.com>
2025-01-18 01:47:03 -0500

    new changes

--- backend/settings.py
+++ backend/settings.py
@@ -4,5 +4,14 @@ from pydantic import BaseModel, Field
 class TwilioCredentials(BaseModel):
     apiKey: str = Field(default_factory=lambda: os.getenv("TWILIO_API_KEY", ""))
 
+class MongoDBSettings(BaseModel):
+    uri: str = Field(default_factory=lambda: os.getenv("MONGO_URL", ""))
+    database_name: str = Field(default_factory=lambda: os.getenv("MONGODB_DB", "phishing_simulator"))
+
+    class JWTSettings(BaseModel):
+        secret: str = Field(default_factory=lambda: os.getenv("JWT_SECRET", ""))
+        algorithm: str = "HS256"
+
 class Settings:
     twilio_credentials: TwilioCredentials = TwilioCredentials()
+    mongodb: MongoDBSettings = MongoDBSettings()
