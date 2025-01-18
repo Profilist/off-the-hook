bd7d13c07a222a6136714cceffc280b85040e04b
Felix Xie <84635387+melon64@users.noreply.github.com>
2025-01-17 22:24:52 -0500

    added initial flask template

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify, session
+from flask_cors import CORS
+from settings import Settings
+from twilio.routes import twilio_routes
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 @app.route('/')
-def home():
-    return jsonify(message="Hello, Flask!")
+def index():
+    return "test"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.register_blueprint(twilio_routes, url_prefix='/email')
