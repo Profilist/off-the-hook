13e47e19961551ba99b9dcf87a3e1a8e66ac4a79
Chrisyhjiang <chrisyh.jiang@gmail.com>
2025-01-18 03:49:35 -0500

    generate log in url and get user info works

--- backend/settings.py
+++ backend/settings.py
@@ -8,10 +8,11 @@ class MongoDBSettings(BaseModel):
     uri: str = Field(default_factory=lambda: os.getenv("MONGO_URL", ""))
     database_name: str = Field(default_factory=lambda: os.getenv("MONGODB_DB", "phishing_simulator"))
 
-    class JWTSettings(BaseModel):
-        secret: str = Field(default_factory=lambda: os.getenv("JWT_SECRET", ""))
-        algorithm: str = "HS256"
+class JWTSettings(BaseModel):
+    secret: str = Field(default_factory=lambda: os.getenv("JWT_SECRET", ""))
+    algorithm: str = "HS256"
 
 class Settings:
     twilio_credentials: TwilioCredentials = TwilioCredentials()
     mongodb: MongoDBSettings = MongoDBSettings()
+    jwt : JWTSettings = JWTSettings()
