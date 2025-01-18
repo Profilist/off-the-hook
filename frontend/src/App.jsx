b874cbfa68e579a5e329cd61a999605bbf39285b
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2025-01-17 22:03:13 -0500

    made fake rbc login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,67 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const [signIn, setSignIn] = useState(true);
+  const toggleSignIn = () => {
+    setSignIn(!signIn);
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {signIn && (
+      <div className="login-container">
+      <div className="background-image">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isVisible && (<div className="login-form">
+        <form>
+          <label className="username">Client Card or Username</label>
+          <input type="text" id="username"/>
+          <div className="checkbox-row">
+            <input type="checkbox" id="save" />
+            <label htmlFor="save">Save client card or username</label>
+          </div>
+          <button type="submit" onClick={toggleVisibility}>Next</button>
+        </form>
+        <div className="links">
+          <a>Recover Your Username</a>
+          <a>Enrol in Online Banking</a>
+        </div>
+        <div className="service-notices">
+          <h4>Service Notices</h4>
+          <a>Weekend maintenance to affect Online/Mobile Banking</a>
+          <a>Other Online Services</a>
+        </div>
+      </div>)}
+
+      {isVisible && ( 
+        <div className="login-form">
+          <form>
+          <label className="password">Password</label>
+          <input type="text" id="username"/>
+          <div className="checkbox-row">
+          </div>
+          <button type="submit" onClick={toggleSignIn}>Sign In</button>
+        </form>
+        <div className="links">
+          <a>Reset Your Password</a>
+          <a>Having Trouble Signing In?</a>
+        </div>
+        <div className="service-notices">
+          <h4>Service Notices</h4>
+          <a>Weekend maintenance to affect Online/Mobile Banking</a>
+          <a>Other Online Services</a>
+        </div>
+        </div>
+      )}
+    </div> )}
+    {!signIn && (<div>Hacker stuff</div>)}
     </>
   )
 }
