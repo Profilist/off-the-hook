f64c4806a4de05ef8d20ba6ab5587a3af0bb3f5d
unknown <larris.xie@gmail.com>
2025-01-18 16:42:26 -0500

    Added React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import Login from './components/phishing/login';
-import Money from './components/phishing/money';
 import Terminal from './components/hacker-side/terminal';
 import Hacked from './components/phishing/hacked';
 import HackerPerspective from './components/hacker-side/hackerperspective';
 import Statistics from './components/hacker-side/statistics';
 
+
 function App() {
   return (
-    <>
-      <HackerPerspective />
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
