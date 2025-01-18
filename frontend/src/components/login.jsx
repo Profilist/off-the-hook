608bf02a40f244a18361bee4665ebb70fc774190
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2025-01-17 22:21:33 -0500

    joined sign in with hacked message

---
+++ frontend/src/components/login.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { useState } from 'react'
+import Hacked from './hacked';
+
+const Login = () => {
+  const [isVisible, setIsVisible] = useState(false);
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
+
+    const [signIn, setSignIn] = useState(true);
+    const toggleSignIn = () => {
+        setSignIn(!signIn);
+    };  
+    return (
+    <>
+    {signIn && (
+      <div className="login-container">
+      <div className="background-image">
+      </div>
+      {!isVisible && (<div className="login-form">
+        <form>
+          <label className="username">Client Card or Username</label>
+          <input type="text" id="username"/>
+          <div className="checkbox-row">
+            <input type="checkbox" id="save" />
+            <label htmlFor="save">Save client card or username</label>
+          </div>
+          <button type="submit" onClick={toggleVisibility}>Next</button>
+        </form>
+        <div className="links">
+          <a>Recover Your Username</a>
+          <a>Enrol in Online Banking</a>
+        </div>
+        <div className="service-notices">
+          <h4>Service Notices</h4>
+          <a>Weekend maintenance to affect Online/Mobile Banking</a>
+          <a>Other Online Services</a>
+        </div>
+      </div>)}
+
+      {isVisible && ( 
+        <div className="login-form">
+          <form>
+          <label className="password">Password</label>
+          <input type="text" id="username"/>
+          <div className="checkbox-row">
+          </div>
+          <button type="submit" onClick={toggleSignIn}>Sign In</button>
+        </form>
+        <div className="links">
+          <a>Reset Your Password</a>
+          <a>Having Trouble Signing In?</a>
+        </div>
+        <div className="service-notices">
+          <h4>Service Notices</h4>
+          <a>Weekend maintenance to affect Online/Mobile Banking</a>
+          <a>Other Online Services</a>
+        </div>
+        </div>
+      )}
+    </div> )}
+
+    {!signIn && (<Hacked />)}
+
+    </>
+  )
+}
+
+export default Login
