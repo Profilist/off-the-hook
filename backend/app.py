9152ab59d878f20fc6a07bece5b1f1b9e7f1bdb8
Felix Xie <84635387+melon64@users.noreply.github.com>
2025-01-18 01:51:12 -0500

    mongo conflict resolved

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 from settings import Settings
 from twilio.routes import twilio_routes
-from mongo.routes import mongo_routes
+from mongodb.routes import mongo_routes
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
